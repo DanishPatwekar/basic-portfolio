3c4d4ea19fa9e2abea5ae04bfcf166a4f3f64cfc
DanishPatwekar <150497435+DanishPatwekar@users.noreply.github.com>
2025-03-06 16:58:12 +0530

    correct js

--- style.js
+++ style.js
@@ -1,7 +1,3 @@
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
   // Select all elements you want to animate
   const animatedDivs = document.querySelectorAll(".education, svg circle");
